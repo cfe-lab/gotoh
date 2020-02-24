ca5b9ae5025455a9ce82a56e33c8e089171b0840
Nathaniel Knight <nknight@cfenet.ubc.ca>
2020-02-24 14:15:41 -0800

    Add Python bindings
    This commit adds a `setup.py` file that lets us build Python bindings
    for the module.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+
+gotoh = Extension(
+    "gotoh",
+    sources=["alignment.cpp"],
+    define_macros=[("__PYTHON__", None)],
+)
+
+setup(
+    name="gotoh",
+    version="0.2",
+    description="Wrapper for Conan's alignment.cpp code",
+    ext_modules=[gotoh],
+    zip_safe=False,
+)

48243b648fde186b1e349aec5c23d2d93fc4a92b
Richard Liang <rliang@cfenet.ubc.ca>
2024-11-01 15:07:35 -0700

    Added a stub test file for filling out.

---
+++ ruby/test/cfe_gotoh_test.rb
@@ -0,0 +1,6 @@
+require_relative 'test_helper'
+require_relative '../lib/cfe_gotoh'
+
+
+class CfeGotohTest < Minitest::Test
+end
---
+++ ruby/test/test_helper.rb
@@ -0,0 +1,29 @@
+require 'simplecov'
+require 'simplecov-cobertura'
+
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter, # Add HTML report for viewing/review.
+    SimpleCov::Formatter::CoberturaFormatter # For CI
+    ])
+end
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+if ENV['CI_COMMIT_TAG']
+  run_id = ENV['CI_COMMIT_TAG']
+elsif ENV["CI"]
+  run_id = "#{ENV['CI_PIPELINE_ID']}-#{ENV['CI_COMMIT_SHORT_SHA']}"
+else
+  run_id = "LOCALBUILD"
+end
+
+Minitest::Reporters.use! [
+  Minitest::Reporters::SpecReporter.new,
+  Minitest::Reporters::JUnitReporter.new,
+  Minitest::Reporters::HtmlReporter.new(
+    :title => "hivdb_algorithm Test Report #{run_id}",
+    :erb_template => File.join(File.dirname(__FILE__), "templates/index.html.erb"))
+]
